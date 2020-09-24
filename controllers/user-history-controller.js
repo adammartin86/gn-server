2319c6c6533ccf0c0d0f064833184e3486a5a1b1
Adam Martin <adam.d.martin1986@gmail.com>
2020-09-24 18:45:39 -0400

    user-history update and delete working

--- controllers/user-history-controller.js
+++ controllers/user-history-controller.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 // let validateSession = require("../middleware/validate-session");
 const UserHistory = require("../db").import("../models/user-history");
 
-
+//FUNCTIONING
 router.post("/post", (req, res) => {
   const userHistory = {
     // owner: req.user.id,
@@ -12,40 +12,42 @@ router.post("/post", (req, res) => {
     difficulty: req.body.userHistory.difficulty,
     winner: req.body.userHistory.winner,
     gameNotes: req.body.userHistory.gameNotes,
-    // gameId: req.userHistory.gameId,
+    gameId: req.body.userHistory.gameId,
   };
   UserHistory.create(userHistory)
     .then((history) => res.status(200).json(history))
     .catch((err) => res.status(500).json({ error: err }));
 });
 
-// router.get("/get/:id", (req, res) => {
-//   let userid = req.user.id.toString();
-//   UserHistory.findAll({
-//     where: { owner: userid },
-//   })
-//     .then((history) => res.status(200).json(history))
-//     .catch((err) => res.status(500).json({ error: err }));
-// });
+router.get("/get/:id", (req, res) => {
+  let userid = req.user.id.toString();
+  UserHistory.findAll({
+    where: { owner: userid },
+  })
+    .then((history) => res.status(200).json(history))
+    .catch((err) => res.status(500).json({ error: err }));
+});
 
+//FUNCTIONING
 router.get("/getall", (req, res) => {
   UserHistory.findAll()
     .then((history) => res.status(200).json(history))
     .catch((err) => res.status(500).json({ error: err }));
 });
 
-router.put("/updatenote/:id", function (req, res) {
+//FUNCTIONING
+router.put("/updatenotes/:id", function (req, res) {
   const updateNote = {
-    // owner: req.user.id,
-    datePlayed: req.body.userHistory.datePlayed,
-    triviaTopic: req.body.userHistory.triviaTopic,
-    difficulty: req.body.userHistory.difficulty,
-    winner: req.body.userHistory.winner,
-    gameNotes: req.body.userHistory.gameNotes,
-    gameId: req.userHistory.gameId,
+      // owner: req.user.id,
+      datePlayed: req.body.userHistory.datePlayed,
+      triviaTopic: req.body.userHistory.triviaTopic,
+      difficulty: req.body.userHistory.difficulty,
+      winner: req.body.userHistory.winner,
+      gameNotes: req.body.userHistory.gameNotes,
+      gameId: req.body.userHistory.gameId,
   };
 
-  const query = { where: { id: req.params.gameId} };
+  const query = { where: { id: req.params.id} };
 
   UserHistory.update(updateNote, query)
     .then((history) => res.status(200).json(history))
@@ -53,7 +55,9 @@ router.put("/updatenote/:id", function (req, res) {
 });
 
 router.delete("/delete/:id", function (req, res) {
-  const query = { where: { id: req.params.id, owner: req.user.id.toString() } };
+  const query = { where: { id: req.params.id, 
+    // owner: req.user.id.toString() 
+  } };
 
   UserHistory.destroy(query)
     .then(() => res.status(200).json({ message: "User History Removed" }))
