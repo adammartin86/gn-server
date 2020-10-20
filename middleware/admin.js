a377123737b0d54e27edd6319276be24ba4eeb13
Jamie Coakley <jamiemcoakley@gmail.com>
2020-10-20 19:46:58 -0400

    added quotes to admin in middleware

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,6 +1,6 @@
 function adminUser() {
     return (req, res, next) => {
-        if(req.user.permission !== admin) {
+        if(req.user.permission !== "admin") {
             res.status(401).send("ADMINS ONLY")
         }
 
