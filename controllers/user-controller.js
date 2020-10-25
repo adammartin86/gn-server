0e5391aab841791e9e1bfc07c398357257ee4f41
alicol <alicol747@gmail.com>
2020-10-25 15:48:16 -0400

    changed back password

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -11,7 +11,7 @@ let sequelize = require("../db");
 router.post('/signup', (req, res) => {
     let userModel = {
         userName: req.body.user.userName,
-        permission: "admin",
+        permission: "basic",
         password: bcrypt.hashSync(req.body.user.password, 14),
     };
     
